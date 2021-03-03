64ca649dc7d454d88dd1a5c7c71c6cd342265c3f
wael ahmed <w.ahmed020@gmail.com>
2021-03-03 09:30:04 +0000

    code update java script

---
+++ app.js
@@ -0,0 +1,16 @@
+//selectors - display
+
+const result = document.getElementById("result");
+const copy = document.getElementById("copy")
+
+//selectors - password options
+
+const length = document.getElementById("length");
+const symbols = document.getElementById("numbers")
+const lowerCase = document.getElementById("lowerCase")
+const upperCase = document.getElementById("upperCase")
+const specialCharacter = document.getElementById("specialCharachter")
+
+//selectors - generate button
+
+const generate = document.getElementById("generate")
